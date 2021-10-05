0493091c09860ceb690c009accc26f302d6c32ab
machilas <marcello.machilas@gmail.com>
2021-10-05 19:52:21 -0300

    CRUD - Tasks

--- src/tasks/shared/task.service.ts
+++ src/tasks/shared/task.service.ts
@@ -1,4 +1,56 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './task';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+    tasks: Task[] = [
+        { id: 1, description: 'Tarefa1', completed: false },
+        { id: 2, description: 'Tarefa2', completed: false },
+        { id: 3, description: 'Tarefa3', completed: true },
+        { id: 4, description: 'Tarefa4', completed: false },
+        { id: 5, description: 'Tarefa5', completed: false },
+        { id: 6, description: 'Tarefa6', completed: false },
+        { id: 7, description: 'Tarefa7', completed: false },
+        { id: 8, description: 'Tarefa8', completed: false },
+        { id: 9, description: 'Tarefa9', completed: false },
+        { id: 10, description: 'Tarefa10', completed: true },
+    ];
+
+    getAll() {
+        return this.tasks;
+    }
+
+    getById(id: number) {
+        const task = this.tasks.find((value) => value.id == id)
+        return task
+    }
+
+    create(task: Task) {
+        let lastId = 0;
+        if (this.tasks.length > 0) {
+            lastId = this.tasks[this.tasks.length - 1].id;
+        }
+
+        task.id = lastId + 1;
+        this.tasks.push(task);
+
+        return task;
+    }
+
+    update(task: Task) {
+        const taskArray = this.getById(task.id)
+        if (taskArray) {
+            taskArray.description = task.description;
+            taskArray.completed = task.completed;
+        }
+
+        return taskArray;
+    }
+
+    delete(id: number) {
+        const index = this.tasks.findIndex((value) => value.id == id);
+        this.tasks.splice(index, 1);
+
+    }
+
+}
